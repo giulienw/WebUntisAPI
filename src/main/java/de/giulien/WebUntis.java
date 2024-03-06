23d9de5589e4df659acc145a9d13cb290acff77f
Giulien <giulienchow@gmail.com>
2024-03-06 09:24:14 +0100

    init

---
+++ src/main/java/de/giulien/WebUntis.java
@@ -0,0 +1,8 @@
+package de.giulien;
+
+import de.giulien.models.WebUntisConfig;
+
+public class WebUntis {
+    private WebUntisConfig _config;
+
+}
---
+++ src/main/java/de/giulien/models/WebUntisConfig.java
@@ -0,0 +1,15 @@
+package de.giulien.models;
+
+public class WebUntisConfig {
+    public String School;
+    public String Server;
+    public String Username;
+    public String Password;
+
+    public WebUntisConfig(String server, String school, String username, String password) {
+        School = school;
+        Server = server;
+        Username = username;
+        Password = password;
+    }
+}
