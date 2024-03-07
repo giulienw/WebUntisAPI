35bfbcf5c9fcda476fc73bc5d8a0975fcdfecee4
Giulien <52256086+giulienw@users.noreply.github.com>
2024-03-07 12:39:08 +0000

    formatting

--- src/main/java/de/giulien/WebUntis.java
+++ src/main/java/de/giulien/WebUntis.java
@@ -19,11 +19,11 @@ import java.util.Map;
 
 public class WebUntis {
     private WebUntisConfig _config;
-    private HttpClient _client ;
+    private HttpClient _client;
     private String _session;
 
     public WebUntis(String username, String password) {
-        _config = new WebUntisConfig("hepta.webuntis.com","besselgym-minden", username, password);
+        _config = new WebUntisConfig("hepta.webuntis.com", "besselgym-minden", username, password);
         try {
             _client = HttpClient.newHttpClient();
 
@@ -33,51 +33,59 @@ public class WebUntis {
     }
 
     public WebUntisUser Login() {
-        WebUntisUser user = Request(WebUntisUser.class,"authenticate", new HashMap<String, String>(){{
-            put("user",_config.Username);
-            put("password", _config.Password);
-            put("client","Default");
-        }});
+        WebUntisUser user = Request(WebUntisUser.class, "authenticate", new HashMap<String, String>() {
+            {
+                put("user", _config.Username);
+                put("password", _config.Password);
+                put("client", "Default");
+            }
+        });
         _session = user.SessionId;
         return user;
     }
 
     public String GetRooms() {
-        if(!ValidateSession()) return null;
+        if (!ValidateSession())
+            return null;
         return Request(String.class, "getRooms", new HashMap<>());
     }
 
     public boolean ValidateSession() {
-        if(_session == null) return false;
+        if (_session == null)
+            return false;
 
         try {
-            Request(String.class,"getLatestImportTime",new HashMap<>());
+            Request(String.class, "getLatestImportTime", new HashMap<>());
             return true;
-        }catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
 
     private <TResponse> TResponse Request(Class<TResponse> tResponse, String method, Map<String, String> params) {
         try {
-            String url = String.format("https://%s/WebUntis/jsonrpc.do?school=%s",_config.Server,_config.School);
-            String param = Utils.ConvertParams(method,params);
+            String url = String.format("https://%s/WebUntis/jsonrpc.do?school=%s", _config.Server, _config.School);
+            String param = Utils.ConvertParams(method, params);
             HttpRequest request = (HttpRequest) HttpRequest.newBuilder(new URI(url))
                     .header("Cookie", "JSESSIONID=" + _session + "; school=" + _config.School)
                     .POST(HttpRequest.BodyPublishers.ofString(param))
                     .build();
             String res = _client.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
-            if(res.contains("error")) APIErrorHandling(res);
+            if (res.contains("error"))
+                APIErrorHandling(res);
+            if (res.contains("["))
+                HandleListResponse(res);
             String[] arr = res.substring(res.indexOf("\"result\":"))
-                    .replace("\"","").replaceAll("result:","")
+                    .replace("\"", "").replaceAll("result:", "")
                     .replace("{", "").replace("}", "")
                     .split(",");
             HashMap<String, String> result = new HashMap<>();
-            for(int i=0;i<arr.length;i++) {
+            for (int i = 0; i < arr.length; i++) {
                 String[] s = arr[i].split(":");
-                if(s.length <= 1)  return (TResponse) s[0];
-                result.put(s[0],s[1]);
+                if (s.length <= 1)
+                    return (TResponse) s[0];
+                result.put(s[0], s[1]);
             }
             return tResponse.getDeclaredConstructor(HashMap.class).newInstance(result);
         } catch (URISyntaxException e) {
@@ -100,12 +108,27 @@ public class WebUntis {
     }
 
     private void HandleListResponse(String res) {
+        String arr[] = res.substring(res.indexOf("\"result\":"))
+                .replace("\"", "").replaceAll("result:", "")
+                .replace("[", "").replace("]", "")
+                .replace("{", "")
+                .replace("}}", "")
+                .split("},");
 
+        HashMap<String, String> result = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            String[] s = arr[i].split(":");
+            if (s.length <= 1)
+                return (TResponse) s[0];
+            result.put(s[0], s[1]);
+        }
+
+        System.out.println(arr[0]);
     }
 
     private void APIErrorHandling(String res) throws WebUntisException {
         String[] arr = res.substring(res.indexOf("\"error\":"))
-                .replace("\"","").replaceAll("error:","")
+                .replace("\"", "").replaceAll("error:", "")
                 .replace("{", "").replace("}", "")
                 .split(",");
 
